e90dca11f3842d7c8c6b3a597a5180f43a446cd0
Justin Young <fuzz.young@gmail.com>
2013-07-06 23:08:20 +1000

    Added python auto complete, and loading extra bashrc stuff

--- install.sh
+++ install.sh
@@ -22,3 +22,13 @@ ln -sf $PWD/git/.gitconfig ~/.gitconfig
 # Link vim configs
 ln -sf $PWD/vim/.vimrc ~/.vimrc
 ln -sf $PWD/vim/.vim ~/.vim
+
+# Link to extra bash stuff
+# Check first if this line is not in the bashrc
+ln -sf $PWD/bash/.bashextras ~/.bashextras
+if ! grep -q "bashextras" ~/.bashrc; then
+    echo "source ~/.bashextras" >> ~/.bashrc 
+fi
+
+# Link python config
+ln -sf $PWD/python/.pythonrc ~/.pythonrc
