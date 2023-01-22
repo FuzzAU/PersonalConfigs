42ce8fe9eb7c1de734b575ffda265de7194ad333
Justin Young <justin@oodar.com.au>
2023-01-22 14:16:55 +1000

    Switched to using starship for command prompt

--- install.sh
+++ install.sh
@@ -18,46 +18,38 @@ fi
 VIMRC=~/.vimrc
 VIMDIR=~/.vim
 GITCONFIG=~/.gitconfig
-PYTHONRC=~/.pythonrc
 BASHEXTRAS=~/.bashextras
 BASHPS1=~/.bashps1
 BASHRC=~/.bashrc
 TMUXCONF=~/.tmux.conf
-DIRCOLORS=~/.dircolors
 # Bash profile for use with OS X
 BASHPROFILE=~/.bash_profile
 MACGITCOMPLETION=~/.git-completion.bash
-POWERLINECONFIG=~/.config/powerline
+STARSHIP=~/.config/starship.toml
+
 
 ## Prompt to install python3
-minimum_required=("python3" "python3-pip")
-read -p "Install python3 & pip for powerline? [Y/n]: " answer
-## Set the default value if no answer was given
-answer=${answer:Y}
-## If the answer matches y or Y, install
-[[ $answer =~ [Yy] ]] && sudo apt-get install ${minimum_required[@]}
+#minimum_required=("python3" "python3-pip")
+#read -p "Install python3 & pip for powerline? [Y/n]: " answer
+### Set the default value if no answer was given
+#answer=${answer:Y}
+### If the answer matches y or Y, install
+#[[ $answer =~ [Yy] ]] && sudo apt-get install ${minimum_required[@]}
 
-## Check to install powerline
-pip_packages=("powerline-status powerline-gitstatus")
-read -p "Install powerline? [Y/n]: " answer
-## Set the default value if no answer was given
-answer=${answer:Y}
-## If the answer matches y or Y, install
-[[ $answer =~ [Yy] ]] && pip3 install ${pip_packages[@]}
+# Install starship
+echo === INSTALLING STARSHIP ===
+curl -sS https://starship.rs/install.sh | sh 
 
-# Create location for powerline
-mkdir -p ~/.config
-install $PWD/powerline/ $POWERLINECONFIG
 
 # Install configuration links
+echo === SYMLINKING CONFIGS ===
 install $PWD/git/gitconfig $GITCONFIG
-#install $PWD/vim/vimrc $VIMRC
-#install $PWD/vim/.vim $VIMDIR
+install $PWD/vim/vimrc $VIMRC
+install $PWD/vim/.vim $VIMDIR
 install $PWD/tmux/tmux.conf $TMUXCONF
-install $PWD/python/pythonrc $PYTHONRC
 install $PWD/bash/bashextras $BASHEXTRAS
-install $PWD/bash/dircolors $DIRCOLORS
-#install $PWD/bash/ps1 $BASHPS1
+install $PWD/starship/starship.toml $STARSHIP
+
 
 # There is some extra work to be done on OS X because it uses bash_profile by default
 # We will attempt to have bash_profile load bashrc
@@ -75,4 +67,4 @@ if ! grep -sq "bashextras" $BASHRC; then
 fi
 
 # Put this reminder in for the git config, because I always forget it
-echo "Remember to update the gitconfig to have the correct e-mail address!!"
+echo "=== Remember to update the gitconfig to have the correct e-mail address!! ==="
